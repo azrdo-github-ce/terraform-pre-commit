575897cec60d2df15552353ac338dcecf6c16c06
Tony Owens <tony.owens@cnb.com>
2020-09-22 16:16:11 -0700

    Adding function to parse out remote-backend.tf and replace the servername found in that file

--- pre_commit_hooks/terraform_module_path_replace.py
+++ pre_commit_hooks/terraform_module_path_replace.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 
 def main():
     dirs = []
@@ -13,15 +14,34 @@ def main():
                             (filename.endswith(".tf") or filename.endswith(".tfvars")):
                                 dirs.append(fullpath)
 
+    serverName = getTFServer()
     for dir in dirs:
         with open(dir, 'r') as file :
             filedata = file.read()
         
-        filedata = filedata.replace('dev.dc04.ptfe.cnb.com', 'localterraform.com')
-        filedata = filedata.replace('prod.dc04.ptfe.cnb.com', 'localterraform.com')
+        filedata = filedata.replace(serverName, 'localterraform.com')
 
         with open (dir, 'w') as file :
             file.write(filedata)
 
 if __name__ == '__main__':
     sys.exit(main())
+
+def getTFServer():
+    for fileName in os.listdir(os.getcwd()):
+        if fileName == "remote-backend.tf":
+            filePath = os.getcwd() + '\\' fileName
+
+            with open(filePath, 'r') as file:
+                toParse = file.read()
+            
+            parsed = toParse.split('\n')
+            for line in parsed:
+                if re.search('hostname', line):
+                    hostname = line.split('=')
+                    hostname = hostname[1].replace('"','')
+                    hostname = hostname.strip()
+
+                    return hostname
+
+            
