45757da918daa8c6cd3fa318a1d2e3e14775ffe4
Tony Owens <tony.owens@cnb.com>
2020-11-05 16:53:35 -0800

    Fixing issue when hook is running from root directory

--- pre_commit_hooks/terraform_module_path_replace.py
+++ pre_commit_hooks/terraform_module_path_replace.py
@@ -3,47 +3,39 @@ import sys
 import re
 
 def getTFServer():
-    for directory in os.listdir("."):
-        if directory != ".git":
-            for directories in os.walk(directory):
-                if ".terraform" not in directories[0]:
-                    for fileName in directories[2]:
-                        if fileName == "remote-backend.tf":
-                            filePath = os.getcwd() + '\\' + directories[0] + '\\' + fileName
+    for fileName in os.listdir("."):
+        if fileName == "remote-backend.tf":
+            filePath = os.getcwd() + '\\' + fileName
 
-                            with open(filePath, 'r') as file:
-                                toParse = file.read()
-                            
-                            parsed = toParse.split('\n')
-                            for line in parsed:
-                                if re.search('hostname', line):
-                                    hostname = line.split('=')
-                                    hostname = hostname[1].replace('"','')
-                                    hostname = hostname.strip()
+            with open(filePath, 'r') as file:
+                toParse = file.read()
+            
+            parsed = toParse.split('\n')
+            for line in parsed:
+                if re.search('hostname', line):
+                    hostname = line.split('=')
+                    hostname = hostname[1].replace('"','')
+                    hostname = hostname.strip()
 
-                                    return hostname
+                    return hostname
 
 def main():
     serverName = getTFServer()
 
-    dirs = []
-    for directory in os.listdir("."):
-        if directory != ".git":
-            for directories in os.walk(directory):
-                if ".terraform" not in directories[0]:
-                    for filename in directories[2]:
-                        fullpath = os.getcwd() + '\\' + directories[0] + '\\' + filename
-                        if fullpath not in dirs and filename != 'remote-backend.tf' and \
-                            (filename.endswith(".tf") or filename.endswith(".tfvars")):
-                                dirs.append(fullpath)
+    files = []
+    for fileName in os.listdir("."):
+        fullpath = os.getcwd() + '\\' + fileName
+        if fullpath not in files and fileName != 'remote-backend.tf' and \
+            (fileName.endswith(".tf") or fileName.endswith(".tfvars")):
+                files.append(fullpath)
 
-    for dir in dirs:
-        with open(dir, 'r') as file :
+    for fileName in files:
+        with open(fileName, 'r') as file :
             filedata = file.read()
-        
+
         filedata = filedata.replace(serverName, 'localterraform.com')
 
-        with open (dir, 'w') as file :
+        with open (fileName, 'w') as file :
             file.write(filedata)
 
 if __name__ == '__main__':
