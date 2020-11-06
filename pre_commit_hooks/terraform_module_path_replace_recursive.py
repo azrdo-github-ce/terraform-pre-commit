08cdfe276dee9be0e077654045c91211417ba830
Tony Owens <tony.owens@cnb.com>
2020-11-06 08:07:58 -0800

    Adding recursive module path replace hook

---
+++ pre_commit_hooks/terraform_module_path_replace_recursive.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import re
+
+def getTFServer():
+    for directory in os.listdir("."):
+        if directory != ".git":
+            for directories in os.walk(directory):
+                if ".terraform" not in directories[0]:
+                    for fileName in directories[2]:
+                        if fileName == "remote-backend.tf":
+                            filePath = os.getcwd() + '\\' + directories[0] + '\\' + fileName
+
+                            with open(filePath, 'r') as file:
+                                toParse = file.read()
+
+                            parsed = toParse.split('\n')
+                            for line in parsed:
+                                if re.search('hostname', line):
+                                    hostname = line.split('=')
+                                    hostname = hostname[1].replace('"','')
+                                    hostname = hostname.strip()
+
+                                    return hostname
+
+def main():
+    serverName = getTFServer()
+
+    dirs = []
+    for directory in os.listdir("."):
+        if directory != ".git":
+            for directories in os.walk(directory):
+                if ".terraform" not in directories[0]:
+                    for filename in directories[2]:
+                        fullpath = os.getcwd() + '\\' + directories[0] + '\\' + filename
+                        if fullpath not in dirs and filename != 'remote-backend.tf' and \
+                            (filename.endswith(".tf") or filename.endswith(".tfvars")):
+                                dirs.append(fullpath)
+
+    for dir in dirs:
+        with open(dir, 'r') as file :              
+            filedata = file.read()
+
+        filedata = filedata.replace(serverName, 'localterraform.com')
+
+        with open (dir, 'w') as file :
+            file.write(filedata)
+
+if __name__ == '__main__':
+    sys.exit(main())
